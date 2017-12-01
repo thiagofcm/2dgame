bf9f29ca789772f66966a5c73bbe9048fab74278
thiagofcm <thiagof1503@gmail.com>
2017-12-01 17:43:24 -0200

    Preparando o painel de game over

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -23,6 +23,21 @@ public Text txtTimer;
 public GameObject hudPanel;
 }
 
+[Serializable]
+public class GameOver {
+
+[Header("Text")]
+public Text txtCoinCount;
+
+public Text txtTimer;
+
+[Header("Other")]
+
+public GameObject GameOverPanel;
+}
+
 public HUD hud;
+
+public GameOver gameOver;
 
 }
